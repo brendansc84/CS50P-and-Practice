147ef138265788d5523d8ee15a23077c6ab2c4e0
Brendan Cormier <brendan.cormier@live.com>
2025-07-29 17:10:13 -0600

    name scrambler

--- Practice/nameScrambler.py
+++ Practice/nameScrambler.py
@@ -2,10 +2,14 @@ import random
 def name_scrambler():
     name = input("Input Name: ")
 
-    letters = list(name)
-    random.shuffle(letters)
-    scrambled = "".join(letters)
-
-    print(scrambled)
+    while True:
+        letters = list(name)
+        random.shuffle(letters)
+        scrambled = "".join(letters)
+        print(scrambled.title())
+    
+        response = input("Press Enter to scramble again, or type 'quit' to stop: ").lower()
+        if response == "quit":
+            break
 
 name_scrambler()
