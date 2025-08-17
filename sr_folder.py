1ab5f9717f63bdc41fcdf09da6395f45f90fd25f
Brendan Cormier <brendan.cormier@live.com>
2025-08-16 18:46:02 -0600

    SR folder code

--- sr_folder.py
+++
@@ -1,4 +0,0 @@
-# Filter emails by SR20250724 or containing zip file or whatever
-# Create folder in main Survey Requests using Subject line
-# Download and extract .zip file contents; dxf file usually
-# Move email to folder
