20866f60f0733e930dda144dfedc73fe7f8119e0
brendansc84 <brendan.cormier@live.com>
2025-08-23 19:40:32 -0600

    old practice

---
+++ pyCrashCourse/5-3.py
@@ -0,0 +1,30 @@
+'''alien_color = "red"
+if alien_color == "green":
+    print("You earned 5 points!")
+elif alien_color == "yellow":
+    print("You earned 10 points!")
+else:
+    print("You earned 15 points!") # elif makes more sense here than else, but play nice.'''
+
+'''age = 14
+
+if age < 2:
+    print("the person is a baby")
+elif 2 <= age <= 4:
+    print("the person is a toddler")
+elif 4 <= age <= 13:
+    print("the person is a kid")
+elif 13 <= age <= 20:
+    print("the person is a teenager")
+elif 20 <= age < 65:
+    print("the person is an adult")
+elif age >= 65:
+    print("the person is an elder")'''
+
+# 5-7
+favourite_fruits = ["apples", "oranges", "pineapples"]
+
+fruit = "apples"
+
+if fruit in favourite_fruits:
+    print(f"you really like {fruit}!")
--- pyCrashCourse/toppings.py
+++ pyCrashCourse/toppings.py
@@ -1,5 +1,8 @@
-requested_topping = ["mushrooms", "onions", "pineapple"]
-if "pepperoni" in requested_topping:
-    print(True)
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
 else:
-    print(False)
+    print("Are you sure you want plain pizza?")
