4959c8ed55932cf9ada760db964d4d3819afaa88
brendansc84 <brendan.cormier@live.com>
2025-08-29 22:33:27 -0600

    PCC dict1

---
+++ pyCrashCourse/person.py
@@ -0,0 +1,10 @@
+cara = {
+    'first name': 'Cara',
+    'last name': 'Gillespie',
+    'age': '35',
+    'city': 'Calgary'
+    }
+print(f"First name: {cara['first name']}")
+print(f"Last name: {cara['last name']}")
+print(f"Age: {cara['age']}")
+print(f"City: {cara['city']}")
