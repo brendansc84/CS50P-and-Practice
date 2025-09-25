704277b622beaf88b168a66ac285bdef7992fb03
Brendan Cormier <brendan.cormier@live.com>
2025-09-24 18:45:32 -0600

    Practice

---
+++ Practice/bluePrint.py
@@ -0,0 +1,6 @@
+class SurveyPoint:
+    def __init__(self, northing, easting, elevation):
+        self.northing = northing
+        self.easting = easting
+        self.elevation = elevation
+        
