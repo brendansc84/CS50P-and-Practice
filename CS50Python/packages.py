d071b87913319f2dd5593e01fa635a0d5cf5fc0c
Brendan Cormier <brendan.cormier@live.com>
2025-10-01 17:24:55 -0600

    Update packages.py

---
+++ CS50Python/packages.py
@@ -0,0 +1,13 @@
+class Package:
+    def __init__(self, number, sender, recipient, weight):
+        self.number = number
+        self.sender = sender
+        self.recipient = recipient
+        self.weight = weight
+
+
+def main():
+    packages = ["Package 1: Alice to Bob, 10kg", "Package 2: Bob to Charlie, 5kg"]
+
+
+main()
